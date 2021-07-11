d4e513182b350a3ee7101714f68e9cb2c651bf11
Sobicus <83043123+Sobicus@users.noreply.github.com>
2021-07-11 20:01:08 +0300

    Updates syntax

--- lesson 10/home work 9/functions.js
+++ lesson 10/home work 9/functions.js
@@ -1,23 +1,23 @@
-let allTdRevenueValue = document.querySelectorAll('[data-id=revenue-value]')
+let allTdRevenueValue = document.querySelectorAll("[data-id=revenue-value]");
 
 function convertToTheNumbers(value) {
-    if(value[value.length - 1] === "B") {
-     return parseInt(value) * 1e9;
-    } else if (value[value.length - 1] === "M") {
-        return parseInt(value) * 1e6;
-    } else {
-        return +value;
-    }
+  if (value[value.length - 1] === "B") {
+    return parseInt(value) * 1e9;
+  } else if (value[value.length - 1] === "M") {
+    return parseInt(value) * 1e6;
+  } else {
+    return +value;
+  }
 }
 
 function bruteForceRevenuevalue() {
-    let accum = 0
-    for(let i = 0; i < allTdRevenueValue.length; i++) {
-        accum += convertToTheNumbers(allTdRevenueValue[i].textContent);
-    }
-    return accum;
+  let accum = 0;
+  for (let i = 0; i < allTdRevenueValue.length; i++) {
+    accum += convertToTheNumbers(allTdRevenueValue[i].textContent);
+  }
+  return accum;
 }
 
 let totalSumRevenueValue = bruteForceRevenuevalue();
-let total = document.querySelector('#summ');
+let total = document.querySelector("#summ");
 total.textContent = `${total.textContent} ${totalSumRevenueValue}`;
