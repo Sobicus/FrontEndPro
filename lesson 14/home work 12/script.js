75ec924d97d1354bf8972c3a7d9beafb0ccd98a4
Sobicus <83043123+Sobicus@users.noreply.github.com>
2021-07-25 20:51:28 +0300

    Update Ho,e tusk 12

--- lesson 14/home work 12/script.js
+++ lesson 14/home work 12/script.js
@@ -1,72 +1,70 @@
 //1
 function setYear() {
-    const yearTextInput = document.querySelector('#text-input');
-    const ourYear = document.querySelector('#title span');
-    ourYear.textContent = yearTextInput.value;
-    yearTextInput.value = "";
+  const yearTextInput = document.querySelector("#text-input");
+  const ourYear = document.querySelector("#title span");
+  ourYear.textContent = yearTextInput.value;
+  yearTextInput.value = "";
 }
 
 //2
 function deleteOurAttr() {
-    document.querySelector('[my-attribute]').removeAttribute('my-attribute');
+  document.querySelector("[my-attribute]").removeAttribute("my-attribute");
 }
 
 //3
-function greenColourAllDataId (e) {
-    const allAttrDataId = document.querySelectorAll('[data-id]');
-    for(let i = 0; i < allAttrDataId.length; i++) {
-        e.stopPropagation();
-        allAttrDataId[i].bgColor = 'green'
-    }
+function greenColourAllDataId(e) {
+  const allAttrDataId = document.querySelectorAll("[data-id]");
+  e.stopPropagation();
+  for (let i = 0; i < allAttrDataId.length; i++) {
+    allAttrDataId[i].bgColor = "green";
+  }
 }
 
 //4
-function myNewRow () {
-    let newTr = document.createElement("tr");
-    newTr.setAttribute("id", "myId");
-    
-    let target = document.getElementsByTagName("tr")[7];
-    target.insertAdjacentElement("beforeBegin", newTr);
+function myNewRow() {
+  let newTr = document.createElement("tr");
+  newTr.setAttribute("id", "myId");
 
+  let target = document.getElementsByTagName("tr")[7];
+  target.insertAdjacentElement("beforeBegin", newTr);
 
-    let newCompany = document.createElement("td");
-    newCompany.textContent = 'Tesla';
+  let newCompany = document.createElement("td");
+  newCompany.textContent = "Tesla";
 
-    let newContact = document.createElement("td");
-    newContact.textContent = 'Elon Musk'
+  let newContact = document.createElement("td");
+  newContact.textContent = "Elon Musk";
 
-    let newRevenue = document.createElement("td");
-    newRevenue.setAttribute("data-id", "revenue-value");
-    newRevenue.textContent = '180M'
-    document.getElementById("myId").appendChild(newCompany);
-    document.getElementById("myId").appendChild(newContact);
-    document.getElementById("myId").appendChild(newRevenue);
+  let newRevenue = document.createElement("td");
+  newRevenue.setAttribute("data-id", "revenue-value");
+  newRevenue.textContent = "180M";
+  document.getElementById("myId").appendChild(newCompany);
+  document.getElementById("myId").appendChild(newContact);
+  document.getElementById("myId").appendChild(newRevenue);
 }
 
 //5
 function DeleteSecondRow() {
-    document.getElementsByTagName('tr')[2].remove();
+  document.querySelector("#title + table tbody").children[2].remove();
 }
-
 
 //6
-function bigStyle () {
-    document.querySelector('#title').style.fontSize = '24px';
+function bigStyle() {
+  document.querySelector("#title").style.fontSize = "24px";
 }
 
-document.addEventListener('DOMContentLoaded', bigStyle);
+document.addEventListener("DOMContentLoaded", bigStyle);
 
 //load
 function load() {
-    const btnDeleteAttr = document.getElementById('removeAttrBtn');
-    btnDeleteAttr.addEventListener( 'click', deleteOurAttr, false);
-    
-    const greenBtn = document.querySelector('#setGreenBtn');
-    greenBtn.addEventListener('click', greenColourAllDataId, true);
+  const btnDeleteAttr = document.getElementById("removeAttrBtn");
+  btnDeleteAttr.addEventListener("click", deleteOurAttr, false);
 
-    const btnAddRow = document.getElementById('addRowBtn');
-    btnAddRow.addEventListener('click', myNewRow);
+  const greenBtn = document.querySelector("#setGreenBtn");
+  greenBtn.addEventListener("click", greenColourAllDataId, true);
 
-    const btnDeleteSecondRow = document.querySelector('#removeSecondRowBtn');
-    btnDeleteSecondRow.addEventListener('click', DeleteSecondRow);
+  const btnAddRow = document.getElementById("addRowBtn");
+  btnAddRow.addEventListener("click", myNewRow);
+
+  const btnDeleteSecondRow = document.querySelector("#removeSecondRowBtn");
+  btnDeleteSecondRow.addEventListener("click", DeleteSecondRow);
 }
