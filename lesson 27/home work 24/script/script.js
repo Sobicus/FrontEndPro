e6249e598a3a92a6df9ac5175c85ca3350ce8b1f
Sobicus <83043123+Sobicus@users.noreply.github.com>
2021-09-12 04:14:15 +0300

    Update home task 24

--- lesson 27/home work 24/script/script.js
+++ lesson 27/home work 24/script/script.js
@@ -31,7 +31,7 @@ let counterSteps = counter();
 
 // Проверка выиграл ли игрок
 function won(arr) {
-  if (arr[arr.length - 1] == "empty") return;
+  if (arr[arr.length - 1] !== "empty") return;
   for (let i = 0; i < arr.length - 1; i++){
       if (i + 1 == arr[i]){ continue; }
       else { return false;}
