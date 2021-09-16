a1241ee25419a6ba29262c952df0b7f39c447923
Sobicus <83043123+Sobicus@users.noreply.github.com>
2021-09-16 09:37:24 +0300

    Home Task 26

---
+++ lesson 31/home work 26/app.js
@@ -0,0 +1,6 @@
+import { calculator }  from './calculator.js';
+
+console.log(calculator.add(10, 2)) // 12
+console.log(calculator.sub(12, 5)) // 7
+console.log(calculator.mult(7, 4)) // 28
+console.log(calculator.div(28, 2)) // 14
---
+++ lesson 31/home work 26/calculator.js
@@ -0,0 +1,27 @@
+import {add} from './add.js';
+import {sub} from './sub.js';
+import {div} from './div.js';
+import {mult} from './mult.js';
+
+
+// export function calculator (){
+//     add(a,b);
+//     sub(a,b);
+//     div(a,b);
+//     mult(a,b);
+// }
+
+// export function calculator (){
+//     add;
+//     sub;
+//     div;
+//     mult;
+// }
+
+// export let calculator = [add(a,b), sub(a,b), div(a,b), mult(a,b)]
+
+// export let calculator = [add, sub, div, mult]
+
+// export let calculator = {add=add(a,b), sub=sub(a,b), div=div(a,b), mult=mult(a,b)}
+
+// export let calculator = {add=add, sub=sub, div=div, mult=mult}
